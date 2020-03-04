5bae22a1926454a7e8e58171b21bfc78bbd6484b
Peter Rose <peterroseq1@gmail.com>
2020-03-04 15:09:41 +0000

    Automatically backed up by Learn

---
+++ lib/while.rb
@@ -0,0 +1,25 @@
+def using_while
+  levitation_force = 6
+  
+  while levitation_force < 10 
+    levitation_force += 1 
+    puts "Wingardium Leviosa"
+  end
+end
+
+
+levitation_force = 6
+
+while levitation_force < 10 
+  levitation_force += 1
+  puts "Wingardium Leviosa"
+end
+
+levitation_force = 6
+  
+while levitation_force < 10 
+  levitation_force += 1 
+  puts "Wingardium Leviosa"
+end
+
+puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
